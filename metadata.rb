f209a1674a78572ffb9268e3bf97399a562c2b50
Lakshmisha <lakshmisha.th@gmail.com>
2019-08-20 05:40:11 +0000

    second commi

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description 'Installs/Configures myiis'
 long_description 'Installs/Configures myiis'
 version '0.1.0'
 chef_version '>= 14.0'
+version '0.2.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- recipes/server.rb
+++ recipes/server.rb
@@ -9,6 +9,10 @@ end
 
 file 'C:\inetpub\wwwroot\Default.htm' do
 	content '<h1>hello, world!<h1>'
+	<h2>platform: #{node['platform']}</h2>
+	<h2>hostname: #{node['hostname']}</h2>
+	<h2>mem: #{node['memory']['total']}</h2>
+	<h2>cpu mhz: #{node['cpu']['0']['mhz']}</h2>
 end
 
 service 'w3svc' do
